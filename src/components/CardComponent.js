97df24b350ac9f09105fd299b8cc9b897949a93f
TurLesh <fresh5932@gmail.com>
2022-06-06 17:14:32 +0300

    List align resposive fix

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -3,7 +3,11 @@ import ReactDom from 'react-dom';
 import '../css/CardStyle.css';
 import '../components/content/CardData'
 
-export default function CardComponent(props){
-    const {item} = props;
-    return <h2>{item}</h2>
+export default function CardComponent({item, itemDescription}){
+    return(
+        <div>
+        <h2>{item}</h2>
+        <h3>{itemDescription}</h3>
+        </div>
+    ) 
 }
