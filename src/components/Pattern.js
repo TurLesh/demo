fc10d57a19103f8538ca97b43ab3b80ead5c809e
Trayaurus <ticinabogdan@gmail.com>
2022-06-11 00:50:41 +0300

    l

--- src/components/Pattern.js
+++ src/components/Pattern.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import "./styles.css";
-import { Count } from "./Count";
-import { Width } from "./Width";
+import '../css/ContactStyle.css'
 
 export default class Counter extends React.Component {
   constructor() {
